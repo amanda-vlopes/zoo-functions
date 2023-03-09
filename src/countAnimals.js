81a1529ae72c985f0cc11698cb42d2240d9b6fec
amanda-vlopes <amandavitor@hotmail.com>
2023-03-09 20:05:30 -0300

    Requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const animais = data.species.reduce((obj, { name, residents }) => {
+    const objRetornado = obj;
+    objRetornado[name] = residents.length;
+    if (animal && animal.species && animal.sex) {
+      objRetornado[name] = residents.filter(({ sex }) => sex === animal.sex).length;
+    }
+    return obj;
+  }, {});
+  if (animal && animal.species) {
+    return animais[animal.species];
+  }
+  return animais;
 };
+
+console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
