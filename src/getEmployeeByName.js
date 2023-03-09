a30827e727e3f6bc9336ffc37ebba5596872154c
amanda-vlopes <amandavitor@hotmail.com>
2023-03-09 16:33:48 -0300

    Requisito 4 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) return {};
+
+  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 };
 
+console.log(getEmployeeByName('Orloff'));
 module.exports = getEmployeeByName;
