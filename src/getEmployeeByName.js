4ad0be4d47e098437b6fcc45a55a7d0a8020d9bf
amanda-vlopes <amandavitor@hotmail.com>
2023-03-09 16:38:05 -0300

    Corrigindo lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,8 +3,8 @@ const data = require('../data/zoo_data');
 const getEmployeeByName = (employeeName) => {
   if (!employeeName) return {};
 
-  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return data.employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 };
 
-console.log(getEmployeeByName('Orloff'));
 module.exports = getEmployeeByName;
