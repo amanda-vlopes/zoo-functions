e8bed32cd4e971b37f3f155466b11892af411d10
amanda-vlopes <amandavitor@hotmail.com>
2023-03-10 12:42:28 -0300

    Requisito 9 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -40,16 +40,4 @@ const calculateEntry = (entrants) => {
   return 0;
 };
 
-// array usado como parâmetro:
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
-console.log(calculateEntry(entrants));
-
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const animais = data.species.map(({ name }) => name);
+const horarios = Object.entries(data.hours);
+const dias = Object.keys(data.hours);
+
+const callback = (result, [dia, horas]) => {
+  const obj = result;
+  obj[dia] = {
+    officeHour: `Open from ${horas.open}am until ${horas.close}pm`,
+    exhibition: data.species.filter(({ availability }) => availability.includes(dia))
+      .map(({ name }) => name),
+  };
+  if (dia === 'Monday') {
+    obj[dia] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  return result;
 };
+
+const getSchedule = (scheduleTarget) => {
+  if (animais.includes(scheduleTarget)) {
+    return data.species.find(({ name }) => name === scheduleTarget).availability;
+  }
+  const agenda = horarios.reduce(callback, {});
+  if (dias.includes(scheduleTarget)) {
+    const obj = {};
+    obj[scheduleTarget] = agenda[scheduleTarget];
+    return obj;
+  }
+  return agenda;
+};
+
+console.log(getSchedule('Tuesday'));
 
 module.exports = getSchedule;
